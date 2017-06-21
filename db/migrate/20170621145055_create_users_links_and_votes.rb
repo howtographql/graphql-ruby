2e1e1b4956ada4ea8a22360245970aebfd0a56a7
Radoslav Stankov <rstankov@gmail.com>
2017-06-21 18:20:48 +0300

    Database - initial schema

---
+++ db/migrate/20170621145055_create_users_links_and_votes.rb
@@ -0,0 +1,24 @@
+class CreateUsersLinksAndVotes < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :name, null: false
+      t.string :email, null: false
+      t.index :email, unique: true
+      t.timestamps
+    end
+
+    create_table :links do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :url, null: false
+      t.string :description, null: false
+      t.timestamps
+    end
+
+    create_table :votes do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :link, null: false, foreign_key: true
+      t.index %i(user_id link_id), unique: true
+      t.timestamps
+    end
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,42 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20170621145055) do
+
+  create_table "links", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "url", null: false
+    t.string "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_links_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "link_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["link_id"], name: "index_votes_on_link_id"
+    t.index ["user_id", "link_id"], name: "index_votes_on_user_id_and_link_id", unique: true
+    t.index ["user_id"], name: "index_votes_on_user_id"
+  end
+
+end
