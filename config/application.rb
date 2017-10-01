3053d87b4654413a20872c3bd28fb803142b9a01
tb <t.bak@selleo.com>
2017-10-01 11:02:55 +0200

    Add CORS to allow connecting to ruby backend from howtographql/react-apollo client

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,13 @@ module GraphqlRuby
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # see https://github.com/cyu/rack-cors
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
