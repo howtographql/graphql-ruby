d85f28ea3fedfc0530e05aa37168d9074287e6ed
Solli Moreira Honorio <shonorio@gmail.com>
2019-04-01 12:45:48 +0200

    updated normalize_filters
    normalize_filters didn't works using "description_contains" and "url_contains" as string, I had to change to use symbols :url_contains and :description_contains to get filter working as well. I'm using Rails 5.2.2.1 and ruby 2.6.2p47

--- app/graphql/resolvers/links_search.rb
+++ app/graphql/resolvers/links_search.rb
@@ -31,8 +31,8 @@ class Resolvers::LinksSearch
 
   def normalize_filters(value, branches = [])
     scope = Link.all
-    scope = scope.like(:description, value['description_contains']) if value['description_contains']
-    scope = scope.like(:url, value['url_contains']) if value['url_contains']
+    scope = scope.like(:description, value[:description_contains]) if value[:description_contains]
+    scope = scope.like(:url, value[:url_contains]) if value[:url_contains]
 
     branches << scope
 
